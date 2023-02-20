d304dfd51da133e45eedcad91bab6a9f8935b2f7
Pierre <pierrelidem@gmail.com>
2023-02-20 14:25:38 +0100

    construction html

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <a href="ajouter.html" class="Btn_add"> <img src="images/plus.png"> Ajouter</a>
+        <table>
+            <tr id="items">
+                <th>Nom</th>
+                <th>Prenom</th>
+                <th>Age</th>
+                <th>Modifier</th>
+                <th>Supprimer</th>
+            </tr>
+            <tr id="">
+                <td>steev</td>
+                <td>johson</td>
+                <td>25 ans</td>
+                <td> <a href="modifier.html"><img src="images/pen.png" alt=""></a> </td>
+                <td> <a href="#"><img src="images/trash.png" alt=""></a> </td>
+
+            </tr>
+        </table>
+
+    </div>
+</body>
+
+</html>
